2759108b61671f02181273ffbff65ce70f68548f
InsomniaNoir <jrshafer@mac.com>
2019-04-17 00:53:49 -0500

    Rails 5 Basics Complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_170936) do
+ActiveRecord::Schema.define(version: 2019_04_16_174319) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
